d4740b4a1fd180d847b8606124dbe2a112ebbe02
MikeLynagh <80194931+MikeLynagh@users.noreply.github.com>
2022-08-06 13:04:52 +0100

    used pokemon api to get data about Pokemon

--- src/components/pokemon/PokemonCards.js
+++ src/components/pokemon/PokemonCards.js
@@ -1,13 +1,50 @@
+import { __esModule } from '@testing-library/jest-dom/dist/matchers';
 import React, { Component } from 'react'
 
+import styled from 'styled-components';
+
+const PokeImg = styled.img`
+width: 5em;
+height: 5em;
+`;
+
+
+
+
 export default class PokemonCards extends Component {
+
+// create state to pull in data from pokemon api
+    state = {
+        name: '',
+        imageUrl: '',
+    }
+
+    // componentDidMount(){
+    //     const { name, url } = this.props;
+    // }
+
+    componentDidMount(){
+        const { name, url } = this.props;
+
+        const imageUrl =   `https://projectpokemon.org/images/normal-sprite/${name}.gif`;
+        this.setState({
+            name, 
+            imageUrl,
+        })
+    }
+
     render() {
+        
+
         return(
             <div className='col-md-3 col-sm-6 mb-5'>
                 <div className='card'>
                     <div className='card-header'>
-                        <h1>Check for card </h1>
+                        <h1> {this.state.name}</h1>
                     </div>
+                    <PokeImg className="mx-auto mt-2">
+                        src = {this.state.imageUrl}
+                    </PokeImg>
                 </div>
             </div>
         )
--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -1,18 +1,43 @@
 import React, { Component } from "react";
 
+
 import PokemonCards from "./PokemonCards";
 
+//import axios for connecting to backend
+import axios from 'axios';
+
 export default class PokemonList extends Component {
+    // import the pokemon api to state 
+    state = {
+        url: 'https://pokeapi.co/api/v2/pokemon/',
+        pokemon: null
+    };
+
+    async componentDidMount(){
+        const res = await axios.get(this.state.url);
+        this.setState({pokemon: res.data['results']});
+
+    }
+
     render(){
         return (
-            <div className="row">
-                <PokemonCards />
-                <PokemonCards />
-                <PokemonCards />
-                <PokemonCards />
-                <PokemonCards />
+                <React.Fragment>
+                    {this.state.pokemon ? (
+                            <div className = "row">
+                                {this.state.pokemon.map(pokemon => (
+                                    <PokemonCards 
+                                    key = {pokemon.name}
+                                    name = {pokemon.name}
+                                    />
+                                ))}
+                            </div>
+                        ) : (
+                            <h1>Loading Pokemon</h1>
+                        )
+                    }
 
-            </div>
+                </React.Fragment>
+
         )
     }
 }
