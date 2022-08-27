52af4949a84ed346a2b464946e56a8c06072606e
MikeLynagh <80194931+MikeLynagh@users.noreply.github.com>
2022-08-27 11:00:51 +0100

    built login function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Header from "./component/Header";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter, Switch } from "react-router-dom";
 import Home from "./component/Home";
 import SearchApp from "./component/SearchApp";
 import Books from "./component/Pokedex/PokemonList";
 import About from "./component/About";
+import Login from "./component/Login/Login";
+import useToken from "./component/Login/useToken";
+
 
 function App(){
+    const { token, setToken } = useToken()
+
+    if(!token){
+        return <Login setToken={setToken} />
+    }
     return(
         <React.Fragment>
             <header>
